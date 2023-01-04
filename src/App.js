ccc2eb3c77d9d85ff1dc8fa581ee1566d4ea508e
X-DSI <xmandsi@gmail.com>
2023-01-04 16:15:49 +0530

    ⏫ UPDATE: Components Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
